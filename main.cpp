b882ea0ff06d359a632add427a26c381bb2b70ea
a.eliseev <AEliseev_vo@inno.tech>
2024-09-26 19:33:22 +0300

    Task 3

---
+++ main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+bool isPalindrome(const std::string &str) {
+    std::stack<char> s; // создаем стек символов
+    const int n = str.length(); // вычисляем длину строки
+
+    for (int i = 0; i < n / 2; i++) {
+        // проходим по первой половине строки
+        s.push(str[i]); // добавляем символ в стек
+    }
+
+    const int start = (n + 1) / 2; // определяем начало второй половины строки
+
+    for (int i = start; i < n; i++) {
+        // проходим по второй половине строки
+        if (str[i] != s.top()) {
+            // если символ не равен верхнему элементу стека
+            return false; // строка не является палиндромом
+        }
+        s.pop(); // удаляем верхний элемент стека
+    }
+
+    return true; // строка является палиндромом
+}
+
+int main() {
+    std::string str; // создаем переменную для хранения строки
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, str); // считываем строку
+    std::cout << str << " is a palindrome: " << std::boolalpha << isPalindrome(str) << '\n';
+    // выводим результат проверки на палиндромность с помощью функции isPalindrome
+    // std::boolalpha используется для вывода bool-значений в виде текста (true/false)
+
+    return 0;
+}
